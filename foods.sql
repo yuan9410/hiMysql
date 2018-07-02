3dc071effd208d1bfc90dc0a65c2da8f46746a9e
yuan <438191837@qq.com>
2018-07-02 19:15:41 +0800

    添加了foods.sql文件

---
+++ foods.sql
@@ -0,0 +1,10 @@
+create table foods(
+id integer primary key auto_increment,
+name varchar(20),price int(10),origin varchar(20));
+
+
+
+insert foods (name,price,origin)
+values('banana','6','South Africa'),('apple','6','America');
+
+
